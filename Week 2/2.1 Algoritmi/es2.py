2905d891b99baa46e0c26382f8061060d8f44713
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-10-03 18:42:56 +0200

    Update es2.py

--- Week 2/2.1 Algoritmi/es2.py
+++ Week 2/2.1 Algoritmi/es2.py
@@ -1,6 +1,9 @@
+# Ask for how many numbers we want to check (number of iterations required)
 n = int(input("Inserisci quanti numeri vuoi: "))
+# Set the first number as the MAX
 MAX = int(input("Inserisci il primo numero: "))
 
+# While loop from 0 to n - 1; i increasing every cycle; print MAX
 i = 0
 while i < n - 1:
     m = int(input("Inserisci un altro numero: "))
