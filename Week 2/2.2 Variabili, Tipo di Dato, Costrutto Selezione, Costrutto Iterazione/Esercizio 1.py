c95d59fc63143f34188909f758e1830ceb286d93
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-10-04 19:37:10 +0200

    Update Esercizio 1.py

--- Week 2/2.2 Variabili, Tipo di Dato, Costrutto Selezione, Costrutto Iterazione/Esercizio 1.py
+++ Week 2/2.2 Variabili, Tipo di Dato, Costrutto Selezione, Costrutto Iterazione/Esercizio 1.py
@@ -1,4 +1,5 @@
-stringa = str(input("Quale frase vediamo oggi? ")).upper()
+# Input salva già in str
+stringa = input("Quale frase vediamo oggi? ").upper()
 # Trova solo le vocali e rimpiazzale in vocali minuscole
 stringanuova = stringa.replace('A', 'a').replace('E', 'e').replace('I','i').replace('O','o').replace('U','u')
 print(stringanuova)
